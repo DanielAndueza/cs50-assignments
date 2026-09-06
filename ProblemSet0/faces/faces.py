9a7e81e620efb81a073d820a962a8072160b52dc
Francisco Andueza <danielanduezap@gmail.com>
2026-09-06 12:28:35 -0600

    feat: first version of faces assignment

---
+++ ProblemSet0/faces/faces.py
@@ -0,0 +1,9 @@
+def main():
+    user_shit = input('put something bitch\n')
+    print('your shit converted peel ana:\n', convert(user_shit))
+
+def convert(user_shit_converted):
+    return user_shit_converted.replace(':)', '🙂').replace(':(', '🙁') 
+
+
+main()
